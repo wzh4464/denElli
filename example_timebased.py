e2c5b938b130910c4833b07f452b7f93eee9179b
wzh4464_fat <32484940+wzh4464@users.noreply.github.com>
2023-10-26 09:38:34 +0800

    paralize

--- example_timebased.py
+++ example_timebased.py
@@ -2,44 +2,43 @@ import big_matrix_cocluster.coclusterSVD as ccSVD
 import big_matrix_cocluster.bicluster as bc
 import numpy as np
 import matplotlib.pyplot as plt
+# Pool
+from multiprocessing import Pool
+import multiprocessing as mp
 
 if __name__ == "__main__":
     # read data/nturgb/video_0.py
     datasize = 600
-    # A = np.load('data/nturgb/video_50.npy')
-    
-    # print(A.shape): (103, 25, 150)
-
-    # coclusterer = ccSVD.coclusterer(A[50, :, :].reshape(25, 150), 25, 150)
-    # result = coclusterer.cocluster(10e-1, 10, 5, True)
-
-    # for frame range(0, 100, 10) do coclustering
     coclusterer = []
-    # for i in range(0, 70, 10):
-    #     A = np.load('data/nturgb/video_' + str(i) + '.npy')
-    #     coclusterer.append(ccSVD.coclusterer(
-    #         A[i, :, :].reshape(A.shape[1], A.shape[2]), A.shape[1], A.shape[2]))
-    #     result = coclusterer[-1].cocluster(10e-1, 3, 5, True)
-    #     print('frame: ' + str(i))
-    #     # print(result)
-    #     print('--------------------------')
-
-    # # draw subplots, left: coclusterer[i].matrix, right: coclusterer[i].newMat
     
-    for i in range(datasize):
+    # for i in range(datasize):
+    #     A = np.load('data/nturgb/video_' + str(i) + '.npy')
+    #     # A (103, 25, 150)
+    #     # reshape to (103, 25*150)
+    #     A = A.reshape(A.shape[0], A.shape[1]*A.shape[2])
+    #     coclusterer.append(ccSVD.coclusterer(
+    #         A, A.shape[0], A.shape[1]))
+    #     result = coclusterer[-1].cocluster(10e-1, 3, 5, True)
+    
+    # parallelized
+    # result is coclusterer list
+    def cocluster(i):
         A = np.load('data/nturgb/video_' + str(i) + '.npy')
         # A (103, 25, 150)
         # reshape to (103, 25*150)
         A = A.reshape(A.shape[0], A.shape[1]*A.shape[2])
         coclusterer.append(ccSVD.coclusterer(
             A, A.shape[0], A.shape[1]))
-        result = coclusterer[-1].cocluster(10e-1, 3, 5, True)
-        print('frame: ' + str(i))
-        # print(result)
-        print('--------------------------')
+        coclusterer[-1].cocluster(10e-1, 3, 5, True)
+        return True
+    
+    with Pool(mp.cpu_count()) as p:
+        p.map(cocluster, range(datasize))
 
     fig, axs = plt.subplots(len(coclusterer), 2)
     for i in range(len(coclusterer)):
         axs[i, 0].imshow(coclusterer[i].matrix)
         axs[i, 1].imshow(coclusterer[i].newMat)
-    plt.show()
+    # plt.show()
+    # save to result/
+    plt.savefig('result/nturgb.png')
