4c6211976a787a3b6251a88abf070b303f6d5a3e
wzh4464_fat <32484940+wzh4464@users.noreply.github.com>
2023-10-30 10:21:42 +0800

    tqdm

--- example_timebased.py
+++ example_timebased.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 # Pool
 from multiprocessing import Pool
 import multiprocessing as mp
+from tqdm import tqdm 
 
 if __name__ == "__main__":
     # read data/nturgb/video_0.py
     datasize = 600
     coclusterer = []
-    PARALLEL = False
+    PARALLEL = True
     # for i in range(datasize):
     #     A = np.load('data/nturgb/video_' + str(i) + '.npy')
     #     # A (103, 25, 150)
@@ -27,22 +28,24 @@ if __name__ == "__main__":
         # A (103, 25, 150)
         # reshape to (103, 25*150)
         A = A.reshape(A.shape[0], A.shape[1]*A.shape[2])
-        coclusterer.append(ccSVD.coclusterer(
-            A, A.shape[0], A.shape[1]))
-        coclusterer[-1].cocluster(10e-1, 3, 5, True)
-        return True
+        coclusterI = ccSVD.coclusterer(
+            A, A.shape[0], A.shape[1])
+        coclusterI.cocluster(10e-1, 3, 5, True)
+        return coclusterI
 
     if PARALLEL:
-        with Pool(mp.cpu_count()-5) as p:
-            p.map(cocluster, range(datasize))
+        with Pool(mp.cpu_count()-4) as p:
+            # p.map(cocluster, range(datasize))
+            # use tqdm to show progress
+            results = list(tqdm(p.imap(cocluster, range(datasize)), total=datasize))
     else:
         # just do it for video_0
         cocluster(0)
 
-    fig, axs = plt.subplots(len(coclusterer), 2)
-    for i in range(len(coclusterer)):
-        axs[i, 0].imshow(coclusterer[i].matrix)
-        axs[i, 1].imshow(coclusterer[i].newMat)
+    fig, axs = plt.subplots(len(results), 2)
+    for i in range(len(results)):
+        axs[i, 0].imshow(results[i].matrix)
+        axs[i, 1].imshow(results[i].newMat)
     # plt.show()
     # save to result/
     plt.savefig('result/nturgb.png')
